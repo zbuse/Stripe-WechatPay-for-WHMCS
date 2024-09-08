ed04722dcfc94685e2ca38e8d775f054895459cb
zbuse <5518208+zbuse@users.noreply.github.com>
2024-09-08 17:00:52 +0800

    Add files via upload

---
+++ modules/gateways/stripewechatpay.php
@@ -0,0 +1,198 @@
+<?php
+session_start();
+use Stripe\StripeClient;
+
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+
+function stripewechatpay_MetaData()
+{
+    return array(
+        'DisplayName' => 'Stripe Wechat_Pay',
+        'APIVersion' => '1.1', // Use API Version 1.1
+        'DisableLocalCreditCardInput' => true,
+        'TokenisedStorage' => false,
+    );
+}
+
+function stripewechatpay_config($params)
+{
+    return array(
+        'FriendlyName' => array(
+            'Type' => 'System',
+            'Value' => 'Stripe Wechat_Pay',
+        ),
+        'StripeSkLive' => array(
+            'FriendlyName' => 'SK_LIVE 密钥',
+            'Type' => 'text',
+            'Size' => 30,
+            'Description' => '填写从Stripe获取到的密钥（SK_LIVE）',
+        ),
+        'StripeWebhookKey' => array(
+            'FriendlyName' => 'Webhook 密钥',
+            'Type' => 'text',
+            'Size' => 30,
+            'Description' => '填写从Stripe获取到的Webhook密钥签名',
+        ),
+        'StripeCurrency' => array(
+            'FriendlyName' => '发起交易货币[可留空]',
+            'Type' => 'text',
+            'Size' => 30,
+            'Description' => '默认获取WHMCS的货币，与您设置的发起交易货币进行汇率转换，再使用转换后的价格和货币向Stripe请求',
+        ),
+        'RefundFixed' => array(
+            'FriendlyName' => '退款扣除固定金额',
+            'Type' => 'text',
+            'Size' => 30,
+      'Default' => '0.00',
+      'Description' => '$'
+        ),
+        'RefundPercent' => array(
+            'FriendlyName' => '退款扣除百分比金额',
+            'Type' => 'text',
+            'Size' => 30,
+      'Default' => '0.00',
+      'Description' => "% <br><br> <div class='alert alert-success' role='alert' style='margin-bottom: 0px;'>Webhook设置 <a href='https://dashboard.stripe.com/webhooks' target='_blank'><span class='glyphicon glyphicon-new-window'></span> Stripe webhooks</a> 侦听的事件:payment_intent.succeeded <br>
+      Stripe webhook " .$params['systemurl']."modules/gateways/stripewechatpay/webhooks.php
+               </div><style>* {font-family: Microsoft YaHei Light , Microsoft YaHei}</style>"
+        )
+    );
+}
+
+function stripewechatpay_link($params)
+{
+  global $_LANG;
+  $amount = ceil($params['amount'] * 100.00);
+  $setcurrency = $params['currency'];
+  $Methodtype = 'wechat_pay';
+  $sessionKey = 'pi_id' . $params['invoiceid'];
+  if ($params['StripeCurrency']) {
+      $exchange = stripewechatpay_exchange($params['currency'], strtoupper($params['StripeCurrency']));
+      if (!$exchange) {
+          return '<div class="alert alert-danger text-center" role="alert">支付汇率错误，请联系客服进行处理</div>';
+      }
+  $amount = floor($params['amount'] * $exchange * 100.00);
+  $setcurrency = $params['StripeCurrency'];
+  }
+
+    try {
+        $stripe = new Stripe\StripeClient($params['StripeSkLive']);
+if (isset($_SESSION[$sessionKey])) {
+    $paymentIntentId = $_SESSION[$sessionKey];
+    $paymentIntent = $stripe->paymentIntents->retrieve($paymentIntentId);
+}
+else
+{
+        $paymentIntent = null;
+        $paymentMethod = $stripe->paymentMethods->create(['type' => $Methodtype]);
+        $paymentIntentParams = [
+        'amount' => $amount,
+        'currency' => $setcurrency, 
+        'payment_method' => $paymentMethod->id,
+        'payment_method_types' => [$Methodtype],
+        'confirmation_method' => 'manual', // 手动确认支付
+        'confirm' => true, // 直接确认支付
+        'return_url' => $params['systemurl'] . 'viewinvoice.php?paymentsuccess=true&id=' . $params['invoiceid'],
+        'description' => $params['companyname'] . $_LANG['invoicenumber'] . $params['invoiceid'],
+        'metadata' => [
+                    'invoice_id' => $params['invoiceid'],
+                    'original_amount' => $params['amount']
+                ],
+            ];
+        $paymentIntentParams['payment_method_options']['wechat_pay']['client']='web';
+        $paymentIntent = $stripe->paymentIntents->create($paymentIntentParams);
+        if (isset($paymentIntent->id)) { $_SESSION[$sessionKey] = $paymentIntent->id; }
+}
+
+
+    } catch (Exception $e) {
+        return '<div class="alert alert-danger text-center" role="alert">支付网关错误，请联系客服进行处理'. $e .'</div>';
+    }
+
+    if ($paymentIntent->status == 'requires_action') {
+        $url = $paymentIntent->next_action->wechat_pay_display_qr_code->image_data_url;
+    }
+        return "
+	<img width='200' src='$url'>
+    <div id='payment-status'>Checking payment status...</div>
+
+    <script>
+    $(document).ready(function() {
+        const paymentIntentId = '$_SESSION[$sessionKey]'; // 通过 PHP 后端动态生成
+        const checkPaymentStatusUrl = '".$params['systemurl']."modules/gateways/stripewechatpay/webhooks.php'; // 处理 PaymentIntent 状态的后端 PHP 脚本
+
+        function checkPaymentStatus() {
+            $.ajax({
+                url: checkPaymentStatusUrl,
+                method: 'POST',
+                data: { check : paymentIntentId },
+                success: function(response) {
+                    const data = JSON.parse(response);
+                    if (data.status === 'requires_action') {
+                        // Payment requires further action; retry checking
+                        setTimeout(checkPaymentStatus, 5000); // Retry every 5 seconds
+                    } else if (data.status === 'succeeded') {
+                        // Payment succeeded; refresh page or show success message
+                        $('#payment-status').text('Payment succeeded! Redirecting...');
+                        setTimeout(function() {
+                            window.location.reload(); // Refresh the page
+                        }, 2000); // Wait 2 seconds before refreshing
+                    } else {
+                        $('#payment-status').text('Payment status: ' + data.status);
+                    }
+                },
+                error: function() {
+                    $('#payment-status').text('Error checking payment status.');
+                }
+            });
+        }
+        // Start checking payment status
+        checkPaymentStatus();
+    });
+    </script>
+        ";
+
+    return '<div class="alert alert-danger text-center" role="alert">发生错误，请创建工单联系客服处理</div>';
+}
+function stripewechatpay_refund($params)
+{
+    $stripe = new Stripe\StripeClient($params['StripeSkLive']);
+    $amount = ($params['amount'] - $params['RefundFixed']) / ($params['RefundPercent'] / 100 + 1);
+    try {
+        $responseData = $stripe->refunds->create([
+            'payment_intent' => $params['transid'],
+            'amount' => $amount * 100.00,
+            'metadata' => [
+                'invoice_id' => $params['invoiceid'],
+                'original_amount' => $params['amount'],
+            ]
+        ]);
+        return array(
+            'status' => ($responseData->status === 'succeeded' || $responseData->status === 'pending') ? 'success' : 'error',
+            'rawdata' => $responseData,
+            'transid' => $params['transid'],
+            'fees' => $params['amount'],
+        );
+    } catch (Exception $e) {
+        return array(
+            'status' => 'error',
+            'rawdata' => $e->getMessage(),
+            'transid' => $params['transid'],
+            'fees' => $params['amount'],
+        );
+    }
+}
+function stripewechatpay_exchange($from, $to)
+{
+    try {
+        $url = 'https://raw.githubusercontent.com/DyAxy/ExchangeRatesTable/main/data.json';
+
+        $result = file_get_contents($url, false);
+        $result = json_decode($result, true);
+        return $result['rates'][strtoupper($to)] / $result['rates'][strtoupper($from)];
+    } catch (Exception $e) {
+        echo "Exchange error: " . $e;
+        return "Exchange error: " . $e;
+    }
+}
---
+++ modules/gateways/stripewechatpay/webhooks.php
@@ -0,0 +1,74 @@
+<?php
+use Stripe\StripeClient;
+use Stripe\Webhook;
+
+
+require_once __DIR__ . '/../../../init.php';
+require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
+require_once __DIR__ . '/../../../includes/invoicefunctions.php';
+
+$gatewayName = 'stripewechatpay';
+$gatewayParams = getGatewayVariables($gatewayName);
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+if (!$gatewayParams['type']) {
+    die("Module Not Activated");
+}
+
+$stripe = new Stripe\StripeClient($gatewayParams['StripeSkLive']);
+
+if (isset($_POST['check'])) {
+		$paymentId = $_POST['check'];
+}
+else {
+if (!isset($_SERVER['HTTP_STRIPE_SIGNATURE'])) {
+    die("错误请求");
+}
+
+$payload = @file_get_contents('php://input');
+$sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+$event = null;
+
+try {
+    $event = Webhook::constructEvent(
+        $payload, $sig_header, $gatewayParams['StripeWebhookKey']
+    );
+    $paymentId = $event->data->object->id;
+} catch(\UnexpectedValueException $e) {
+    logTransaction($gatewayParams['paymentmethod'], $e, $gatewayName.': Invalid payload');
+    http_response_code(400);
+    exit();
+} catch(Stripe\Exception\SignatureVerificationException $e) {
+    logTransaction($gatewayParams['paymentmethod'], $e, $gatewayName.': Invalid signature');
+    http_response_code(400);
+    exit();
+}
+}
+
+try {
+        $paymentIntent = $stripe->paymentIntents->retrieve($paymentId,[]);
+        if ($paymentIntent->status == 'succeeded') {
+            $invoiceId = checkCbInvoiceID($paymentIntent['metadata']['invoice_id'], $gatewayParams['paymentmethod']);
+			checkCbTransID($paymentId);
+       //     echo "Pass the checkCbTransID check\n";
+            logTransaction($gatewayParams['paymentmethod'], $paymentIntent, $gatewayName.': Callback successful');
+            addInvoicePayment(
+                $invoiceId,
+                $paymentId,
+                $paymentIntent['metadata']['original_amount'],
+                0,
+                $params['paymentmethod']
+            );
+//            echo "succeeded\n";
+	}
+	    echo json_encode(['status' => $paymentIntent->status ]);
+    
+} catch (Exception $e) {
+    logTransaction($gatewayParams['paymentmethod'], $e, 'error-callback');
+    http_response_code(400);
+    echo $e;
+}
+
+
+
